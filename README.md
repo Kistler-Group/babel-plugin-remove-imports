d4768a8784ba79fa98a310ae211dcc0cf51a7961
Marian Ban <marian.ban@kistler.com>
2015-12-21 09:46:40 +0100

    make it work as a standard plugin defined in babelrc

--- README.md
+++ README.md
@@ -2,22 +2,12 @@
 
 Remove unwanted `import` declarations when building packages with babel transforms.
 
-## Installation
-
-```sh
-$ npm install -D babel-plugin-remove-imports
-```
-
 ## Usage
 
-This plugin currently working only via the Node API
+Babelrc configuration
 
 ```javascript
-var RemoveImports = require("babel-plugin-remove-imports");
-var regexp = new RegExp(/\S+(\.scss$)/g);
-require("babel-core").transform("code", {
-  plugins: [RemoveImports(regexp)]
-});
+{
+  ["babel-plugin-remove-imports", ["\\S+(\\.scss$)"]]
+}
 ```
-
-When `regexp` can be an Array of RegExp
--- index.js
+++ index.js
@@ -5,29 +5,41 @@
  *  PLEASE USE WITH COUTION and check your RegExp expressions carefully.
  */
 
-module.exports = function (regex) {
-    return function(babel) {
-        return new babel.Transformer('babel-plugin-remove-imports', {
-            ImportDeclaration: function (node, parent) {
+'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+exports.default = function () {
+    return {
+        visitor: {
+            ImportDeclaration: function (nodePath, state) {
                 //node.source.value contains the import declaration
-                var text = node.source.value;
+                var text = nodePath.node.source.value;
 
-                //convert regex to Array of regexp if we have a single item
-                if(!(regex instanceof Array)) {
+                //plugin arguments
+                var regex = state.opts;
+
+                if (!(regex instanceof Array)) {
                     regex = [regex];
                 }
 
+                //convert string regexp patterns to instances of RegExp
+                regex = regex.map(function(r) {
+                    return typeof r === 'string' ? new RegExp(r) : r;
+                });
+
                 //iterate over all regexps to find a truthy one,
                 //when found, remove the `import` node from the code
-                for(var i = 0, len = regex.length; i < len; i++) {
+                for(var i = 0; i < regex.length; i++) {
                     if(isRegexExpressionTruthy(text, regex[i])) {
-                        this.dangerouslyRemove();
+                        nodePath.remove();
                         break;
                     }
                 }
             }
-        });
+        }
     }
 };
 
